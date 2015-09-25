dbda86719677469d0d16e86cd323d1b00980b360
gogobebe2 <gogobebe2@gmail.com>
2015-09-25 20:35:59 +1200

    1.0.0-DEVELOPMENT-SNAPSHOT

---
+++ src/main/java/com/gmail/gogobebe2/gunslinger/Gunslinger.java
@@ -0,0 +1,15 @@
+package com.gmail.gogobebe2.gunslinger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Gunslinger extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Starting up " + this.getName() + ". If you need me to update this plugin, email at gogobebe2@gmail.com");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabling " + this.getName() + ". If you need me to update this plugin, email at gogobebe2@gmail.com");
+    }
+}
